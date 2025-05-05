93379f9147f8f4a4c47e58a659ac6698a501a636
Abdelrahman Bauomyi <abdo01820@gmail.com>
2025-05-05 07:56:43 +0300

    First Commit - Spring Init

---
+++ src/test/java/com/example/Job/Tracker/JobTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Job.Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
